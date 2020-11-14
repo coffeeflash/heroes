e9177aa5454b7eb54a7b2623b8300d5deed572d5
Schneeberger Tobias <tobiassamuel.schneeberger@students.bfh.ch>
2020-11-14 11:20:01 +0100

    rebuilt project with spring-boot

--- camp/src/main/java/ch/bfh/coffeeflash/camp/service/PartyService.java
+++
@@ -1,8 +0,0 @@
-package ch.bfh.coffeeflash.camp.service;
-
-import ch.bfh.coffeeflash.camp.model.Party;
-
-public interface PartyService {
-
-    Party createParty(String name);
-}
---
+++ src/test/java/ch/bfh/coffeeflash/heroes/HeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bfh.coffeeflash.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
